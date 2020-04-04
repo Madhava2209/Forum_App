631516b9c11fc951b29be137e6ce09c77a4ac6d3
Madhava2209 <52972972+Madhava2209@users.noreply.github.com>
2020-04-04 20:06:39 +0530

    DONE

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import dashboard,questions
+from .views import dashboard,questions,discussion,upvote
 urlpatterns = [
     path('dashboard/',dashboard),
     path('questions/',questions),
+    path('discussion/<int:discussion_id>/',discussion),
+    path('upvote/<int:discussion_id>/',upvote),
 ]
--- forum/views.py
+++ forum/views.py
@@ -4,8 +4,8 @@ from .models import Answer,Question,Upvote
 
 
 def dashboard(request):
-    all_question=Question.objects.all()
-    all_answer=Answer.objects.all()
+    all_question=Question.objects.all().order_by("-timestamp")
+    all_answer=Answer.objects.all().order_by("-timestamp")
     return render(request,"dashboard.html",{"question":all_question,"answer":all_answer})
 
 
@@ -18,3 +18,32 @@ def questions(request):
         return redirect("/dashboard/")
 
     return render(request,"question.html",{"question":all_question})
+
+def discussion(request,discussion_id):
+    question_instance=Question.objects.get(pk=discussion_id)
+    disc=Answer.objects.filter(question=question_instance)
+    if request.method=="POST":
+        answer=request.POST["answer"]
+        author=request.user
+        upvotes=1
+        question=question_instance
+        answer_instance=Answer.objects.create(
+            answer=answer,
+            author=author,
+            question=question,
+            upvotes=upvotes
+        )
+        return redirect(f"/discussion/{discussion_id}/")
+
+    return render(request,"discussion.html",{"question":question_instance,"discussion":disc})
+
+def upvote(request,discussion_id):
+    answer=Answer.objects.get(pk=discussion_id)
+    upvotes=Upvote.objects.filter(reader=request.user,answer=answer)
+    if upvotes :
+        return redirect("/dashboard/")
+    answer.upvotes +=1
+    answer.save()
+    upvotess=Upvote.objects.create(reader=request.user,answer=answer)
+    return redirect(f"/discussion/{discussion_id}/")
+
